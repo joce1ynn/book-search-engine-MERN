9476a9845575f31e630d069ae7b77baa48fdb550
Wenwen Tian <wwtian9@gmail.com>
2022-03-26 10:45:37 -0600

    add schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,11 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User, Book } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const resolvers = {
+  Query: {},
+
+  Mutation: {},
+};
+
+module.exports = resolvers;
